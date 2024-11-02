1d75af56fdd3a38e6d8928a19e7b155b0ab3ed4e
Muaaweyah Ibn Noufal K.P <muaaweyahkp@gmail.com>
2024-11-02 16:54:47 +0530

    operators in js

---
+++ operators.js
@@ -0,0 +1,16 @@
+//  Arithmetics
+
+//  Addition
+console.log(5+8);
+
+//  Subtraction
+console.log(8-3);
+
+//  Multiplication
+console.log(5*8);
+
+//  Division
+console.log(8/2);
+
+//  Reminder
+console.log(9%2);
